e449679d81d6f7768698be7d4cf1e4f3ca333a6c
Ahmed Swerky <ahmedelswerkey@gmail.com>
2023-12-05 07:44:07 +0000

    Revert "v1.1.4 - upgrade to react 18"
    This reverts commit bf68459faeb182720af7667b972b9e632f1189f4.

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-flying-item'
+import 'react-flying-item/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
